b8a6050d672697a92365e3d494d5800f32337283
Cheng Yanru <yanrcheng2@gmail.com>
2020-02-21 12:31:56 +0800

    Fix DFS, using dict for exploration duplication.

--- crash_calculator.py
+++ crash_calculator.py
@@ -2,27 +2,28 @@ import argparse
 from button import Button
 
 class CrashCalculator:
-    def __init__(self, args):
-        self.register = args.register
-        self.moves = args.moves
-        self.target = args.target
-        self.buttons = [Button(name) for name in args.buttons]
+    def __init__(self, register, moves, target, buttons):
+        self.register = register
+        self.moves = moves
+        self.target = target
+        self.buttons = buttons
 
     def solve(self): # DFS
-        # state: (current register, steps left, actions taken)
-        dp = [(self.register, self.moves, [])]
-        met = set([self.register])
+        # state: (current register, actions left)
+        dp = [(self.register, [])]
+        d = {self.register: self.moves}
         while dp:
-            reg, steps, actions = dp.pop()
+            reg, actions = dp.pop()
+            step = d[reg]
+            if step <= 0: continue
 
-            if steps <= 0: continue
             for b in self.buttons:
                 new_reg = b.func(reg)
                 if new_reg == self.target:
                     return actions + [b.name]
-                if new_reg in met: continue
-                met.add(new_reg)
-                dp.append((new_reg, steps - 1, actions + [b.name]))
+                if new_reg in d and d[new_reg] >= step - 1: continue
+                d[new_reg] = step - 1
+                dp.append((new_reg, actions + [b.name]))
         return ['Fail']
 
 def get_args():
@@ -35,5 +36,7 @@ def get_args():
 
 if __name__ == '__main__':
     args = get_args()
-    cc = CrashCalculator(args)
+    register, moves, target = args.register, args.moves, args.target
+    buttons = [Button(name) for name in args.buttons]
+    cc = CrashCalculator(register, moves, target, buttons)
     print(cc.solve())
